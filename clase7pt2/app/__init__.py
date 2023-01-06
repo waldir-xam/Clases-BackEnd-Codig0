251255775b50b26ef16dea755a90b4f30738c6cb
Waldir <waliuxd@gmail.com>
2023-01-05 21:03:13 -0500

    sql

--- clase7pt2/app/__init__.py
+++ clase7pt2/app/__init__.py
@@ -1,8 +1,11 @@
+from os import getenv
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from config import environment
 
 app = Flask(__name__)
+app.config.from_object(environment[getenv('ENVIRONMENT')])
 
 api = Api(
     app,
---
+++ clase7pt2/app/models/roles_model.py
@@ -0,0 +1,4 @@
+from app.models.base import BaseModel
+
+class RoleModel(BaseModel):
+  pass
